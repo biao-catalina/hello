d7621b268229e55ef00c97a70f9038941032301f
chengbiao <chengbiao@gome.com.cn>
2021-05-17 17:26:28 +0800

    extension modify

--- extension/fillWorkTime/popup.js
+++ extension/fillWorkTime/popup.js
@@ -1,8 +1,6 @@
-chrome.tabs.query({active: true}, function (tabs) {
-    var tab = tabs[0];
-    /*    chrome.tabs.executeScript(tab.id, {
-            file: "askForLeave.js"
-        }, null);*/
 
-    chrome.tabs.executeScript(tab.id, {file: "askForLeave.js"});
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.tabs.executeScript(null, {
+        file: "askForLeave.js"
+    });
 });
--- extension/getWorkTime/popup.js
+++ extension/getWorkTime/popup.js
@@ -1,15 +1,5 @@
-let tab_title = '';
-
-function display_h1(results) {
-    // alert(results)
-    // document.querySelector("#id1").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
-}
-
-chrome.tabs.query({active: true}, function (tabs) {
-    var tab = tabs[0];
-
-    tab_title = tab.title;
-    chrome.tabs.executeScript(tab.id, {
+document.addEventListener("DOMContentLoaded", function () {
+    chrome.tabs.executeScript(null, {
         file: "getWorkTime.js"
-    }, display_h1);
+    });
 });
